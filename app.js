1bab16c90ec98ef38820b422fd1dc29e95a7aa09
tomwecke <twecke@wecke-consulting.de>
2025-06-17 02:06:21 +0200

    Test

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navbarLinks = document.querySelectorAll('.navbar__links'); // Alle Links in der Navbar
 
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+// Event Listener für jeden Link in der Navbar
+navbarLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        menu.classList.remove('is-active'); // Entfernt die aktive Klasse vom Menü
+        menuLinks.classList.remove('active'); // Schließt die Navbar
+    });
 });
 
